101a32cac7c510bfcd1274407bf1aea061bb4ec4
mingri31164 <3116430062@qq.com>
2025-05-02 04:04:39 +0800

    feat：future获取线程返回结果

--- src/main/java/com/mingri/threadpool/TestCountDownLatch.java
+++ src/main/java/com/mingri/threadpool/TestCountDownLatch.java
@@ -1,17 +1,24 @@
 package com.mingri.threadpool;
 
 import java.util.Arrays;
+import java.util.Map;
 import java.util.Random;
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.concurrent.*;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.client.RestTemplate;
 
 /**
  * 用来进行线程同步协作，等待所有线程完成倒计时
  **/
+@Slf4j
 public class TestCountDownLatch {
 
     public static void main(String[] args) throws InterruptedException {
+        test2();
+    }
+
+    private static void test2() throws InterruptedException {
         ExecutorService service  = Executors.newFixedThreadPool(10);
         CountDownLatch latch = new CountDownLatch(10);
         Random r = new Random();
@@ -34,6 +41,42 @@ public class TestCountDownLatch {
         }
         latch.await();
         System.out.println("\n欢迎来到王者荣耀");
+        service.shutdown();
+    }
+
+    private static void test3() throws InterruptedException, ExecutionException {
+        RestTemplate restTemplate = new RestTemplate();
+        log.debug("begin");
+        ExecutorService service = Executors.newCachedThreadPool();
+        CountDownLatch latch = new CountDownLatch(4);
+        // 若想让主线程获取返回结果应使用future
+        Future<Map<String,Object>> f1 = service.submit(() -> {
+            Map<String, Object> response = restTemplate.getForObject("http://localhost:8080/order/{1}", Map.class, 1);
+//            latch.countDown();
+            return response;
+        });
+        Future<Map<String, Object>> f2 = service.submit(() -> {
+            Map<String, Object> response1 = restTemplate.getForObject("http://localhost:8080/product/{1}", Map.class, 1);
+//            latch.countDown();
+            return response1;
+        });
+        Future<Map<String, Object>> f3 = service.submit(() -> {
+            Map<String, Object> response1 = restTemplate.getForObject("http://localhost:8080/product/{1}", Map.class, 2);
+//            latch.countDown();
+            return response1;
+        });
+        Future<Map<String, Object>> f4 = service.submit(() -> {
+            Map<String, Object> response3 = restTemplate.getForObject("http://localhost:8080/logistics/{1}", Map.class, 1);
+//            latch.countDown();
+            return response3;
+        });
+
+//        latch.await();
+        System.out.println(f1.get());
+        System.out.println(f2.get());
+        System.out.println(f3.get());
+        System.out.println(f4.get());
+        log.debug("执行完毕");
         service.shutdown();
     }
 
